77d3c0bcd899ec2c5f4fd44fcb3bfb8a7bbea408
freddy0795mh <freddymh07952019@gmail.com>
2022-05-30 18:50:23 -0500

    Creando proyecto Unitek

---
+++ login-page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pagina web streaming de peliculas">
+    <meta name="author" content="Freddy Muñoz">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <title>Films+</title>
+</head>
+<body class="body-login-page-php">
+    <div class="d-flex justify-content-center p-5 login-page-form text-white">
+        <div>
+            <h1 class="mt-5 mb-4 text-center">Films+</h1>
+            <h4 class="mb-3">Iniciar sesión con tu correo</h4>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control p-3" placeholder="Ingrese su correo" aria-label="Ingrese su correo" aria-describedby="basic-addon2">
+                <span class="input-group-text" id="basic-addon2">@example.com</span>
+            </div>
+            <div class="input-group mb-3">
+                <input type="password" class="form-control p-3" placeholder="Ingrese su contraseña" aria-label="contraseña" aria-describedby="basic-addon2">
+                <span class="input-group-text" id="basic-addon2"><i class="bi bi-chat-square-dots-fill"></i></span>
+            </div>
+            <button type="button" class="btn btn-primary btn-lg w-100 mt-3">Iniciar Sesion</button>
+            <div>
+                <h4 class="mt-4 fs-6 text-secondary">¿No estas registrado en Films+? <a href="register-page.php" class="nav-item text-white text-decoration-none" title="Registrate">Registrate</a></h4>
+            </div>
+        </div>
+    </div>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ register-page.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pagina web streaming de peliculas">
+    <meta name="author" content="Freddy Muñoz">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <title>Films+</title>
+</head>
+<body class="body-login-page-php">
+    <div class="d-flex justify-content-center p-5 login-page-form text-white">
+        <div class="w-50">
+            <h1 class="mt-5 mb-4 text-center">Films+</h1>
+            <h4 class="mb-3">Ingresa tus datos personales</h4>
+            <div class="d-flex flex-wrap justify-content-center align-items-center">
+                <div class="mb-3 w-50 p-2 ps-0">
+                    <h3>Nombres</h3>
+                    <div class="input-group">
+                        <input type="text" class="form-control p-3" placeholder="Ingrese su correo" aria-label="Ingrese su correo" aria-describedby="basic-addon2">
+                        <span class="input-group-text" id="basic-addon2">*</span>
+                    </div>
+                </div>
+                <div class="mb-3 w-50 p-2 ps-0">
+                    <h3>Apellidos</h3>
+                    <div class="input-group">
+                        <input type="text" class="form-control p-3" placeholder="Ingrese su correo" aria-label="Ingrese su correo" aria-describedby="basic-addon2">
+                        <span class="input-group-text" id="basic-addon2">*</span>
+                    </div>
+                </div>
+            </div>
+                <button type="button" class="btn btn-primary btn-lg w-100 mt-3">Iniciar Sesion</button>
+            <div>
+                <h4 class="mt-4 fs-6 text-secondary">¿Ya tienes cuenta? <a href="login-page.php" class="nav-item text-white text-decoration-none" title="Registrate">Inicia Sesión</a></h4>
+            </div>
+        </div>
+    </div>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
